a05a31b413a3e505e7c59df88ddd27f988d6b877
Thammarong Glomjai <thammrong@gmail.com>
2022-08-25 03:21:24 +0700

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  google_client_id:
-    '1095292279361-pgqkihqqt2hbbrnmrmol0rr2udcv6oip.apps.googleusercontent.com',
-  google_client_secret: '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',
-  google_scope: 'profile email',
-};
